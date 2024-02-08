3bae250892e07533bd8cbea2d26a450ca39335f9
subhadip-kundu <subhadipkundu106@gmail.com>
2024-02-08 17:46:35 +0530

    Redux store added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./Appwrite/userAuth.js";
+import "./App.css";
+import { login, logout } from "./Store/authSlice.js";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
     <>
-      
+      <div>Going with the BlogPlaza</div>
     </>
-  )
+  ) : null;
 }
 
-export default App
+export default App;
---
+++ src/Appwrite/userAuth.js
@@ -0,0 +1,64 @@
+import { Client, Account, ID } from "appwrite";
+import config from "../Config/Config";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.createAccount(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
+
+
+
